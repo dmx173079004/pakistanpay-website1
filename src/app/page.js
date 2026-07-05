1f13707cb9dbe2246338488e4573c7642ab1bbbf
dmx173079004 <46341530+dmx173079004@users.noreply.github.com>
2026-07-06 03:10:45 +0800

    Delete src directory

--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getDefaultCountryCode } from '@/lib/utils';
-
-export default function RootPage() {
-  const defaultCountry = getDefaultCountryCode();
-  redirect(`/${defaultCountry}/`);
-}
